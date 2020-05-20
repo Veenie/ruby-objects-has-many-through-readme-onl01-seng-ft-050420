89c985dbaadc3321cd3ffb2f5988a159a59563c7
Raveen K Sharma <raveen08@gmail.com>
2020-05-20 21:55:04 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -15,16 +15,17 @@ class Waiter
  def self.all
    @@all
  end
-  
-  def meals
+ 
+ def meals
     Meal.all.select do |meal|
       meal.waiter == self #checking for waiter now
-  end
+    end  
+ end
   
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
-    end
+  end
  
     best_tipped_meal.customer
   end
